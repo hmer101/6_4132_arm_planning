f55f49aaa05c33f2eac76408d89f353ec4238ed3
Harvey <hmer101@mit.edu>
2022-12-07 11:38:16 -0500

    added part3 file

---
+++ part3_traj_opt.py
@@ -0,0 +1,12 @@
+from pydrake.solvers import MathematicalProgram
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    prog = MathematicalProgram()
+    
+
+
+if __name__ == '__main__':
+    main()
