02f7fb1b1a50e5a4eedb8a5f3cffb77cf67a17a2
Kevin Becker <kevin00@mit.edu>
2022-12-07 11:44:41 -0800

    Inital implementation of the trajectory optimization algorithm

--- part3_traj_opt_kevin.py
+++ part3_traj_opt_kevin.py
@@ -4,42 +4,104 @@ import matplotlib.pyplot as plt
 import pydrake.solvers
 from pybullet_tools.utils import get_link_pose, get_joint_position, get_joint_positions
 from utils import all_poses_from_config
+from rrt import detect_collision
 
 class Trajectory():
-    def __init__(self, world, config_comfortable=(0,0,0,0,0,0,0)):
+    def __init__(self, world, steps, init_config):
         self.prog = MathematicalProgram()
-        self.c_len = len(config_comfortable)
-        self.config_comfortable = config_comfortable
+        self.c_len = 7 #len(config_comfortable)
+        #self.config_comfortable = config_comfortable
+        self.N = steps
         self.vars = self.prog.NewContinuousVariables(self.c_len)
         self.world = world #PLEASE pass by refrence...
+        self.init_config = init_config
 
 
     def optimize(self, config_init):
 
-        #def cost_fun(config):
-        #    return (sum([(config[i]-self.config_comfortable[i])**2 for i in range(len(self.c_len))]))**0.5
-
 
         # Reduces the deviation from the arms "comfortable" config
-        self.prog.AddQuadraticErrorCost(
-            Q=np.array(np.diag([x for x in range(self.c_len)])), # weight each joint slightly differently
-            x_desired=np.array(self.config_comfortable),
+        ####################################
+        # self.prog.AddQuadraticErrorCost(
+        #     Q=np.array(np.diag([x for x in range(self.c_len)])), # weight each joint slightly differently
+        #     x_desired=np.array(self.config_comfortable),
+        #     vars=self.vars
+        # )
+
+        # Reduces the deviation from the arms "comfortable" config
+        ####################################
+        # self.prog.AddQuadraticErrorCost(
+        #     Q=np.array(np.diag([x for x in range(self.c_len)])), # weight each joint slightly differently
+        #     x_desired=np.array(self.config_comfortable),
+        #     vars=self.vars
+        # )
+
+        # Constrains the joint positions to their min and max values
+        ####################################
+        # self.prog.AddConstraint(
+        #     lambda config: all_poses_from_config(self.world.robot, config),
+        #     lb=[0, 0],
+        #     ub=[np.inf, np.inf],
+        #     vars=self.vars)
+        
+        # Detects collisions
+        ####################################
+        # self.prog.AddConstraint(
+        #     lambda config: detect_collision(self.world.robot, config),
+        #     lb=np.NINF,
+        #     ub=0
+        #     vars=self.vars
+        # )
+
+        
+
+        # Returns 1 once it has a collision, or (hopefully) returns 0
+        ####################################
+        def has_collision(in_vars):
+            # goes from vars=[q0|q1|...|qn] to [q1, q2, ..., qn]
+            configs = np.reshape(in_vars, (self.c_len, self.N))
+            for config in configs:
+                if detect_collision(self.world.robot, config):
+                    return 1
+            return 0
+        
+
+        # Total config space distance traveled, sums over config1, config2, ...
+        ####################################
+        def config_dist(in_vars):
+            configs = np.reshape(in_vars, (self.c_len, self.N))
+            return sum([c_dist(configs[i+1],configs[i]) for i in range(self.N-1)])
+            
+        # Root-Square error between 2 configs 
+        ####################################
+        def c_dist(c1, c2):
+            return (sum([(c1[i+1]-c2[i])**2 for i in range(self.c_len)]))**0.5
+
+
+        
+
+        # Detect collision for vars=[q0|q1|...|qn]
+        ####################################
+        self.prog.AddConstraint(
+            lambda config_vector: has_collision(config_vector),
+            lb=np.NINF,
+            ub=0
             vars=self.vars
         )
 
-        # Constrains the joint positions to their min and max values
-        self.prog.AddConstraint(
-            lambda poses: all_poses_from_config(world.robot, config),
-            lb=[0, 0],
-            ub=[np.inf, np.inf],
-            vars=self.vars)
-        
-
-        result = pydrake.solvers.Solve(self.prog, np.array(config_init)))
+        # Minimize the travel cost
+        ###################################
+        self.prog.AddCost(lambda z: config_dist(z), vars = self.vars)
 
 
 
-def main():
+        result = pydrake.solvers.Solve(self.prog, np.array(config_init))
 
-if __name__ == '__main__':
-    main()
+        return result
+
+
+
+# def main():
+
+# if __name__ == '__main__':
+#     main()
